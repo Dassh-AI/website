5504941c27e79aeb12a07676031a74b8e67b1155
Shivansh Singh <shivanshsingh1910@gmail.com>
2024-08-24 01:22:27 +0530

    added icon

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,20 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "@mantine/core/styles.css";
 import Compose, { IComposeProps } from "./components/layout/compose";
 import Router from "./pages/router";
+import ReactGA from "react-ga4";
 import "animate.css";
 
 const App = () => {
   const queryClient = new QueryClient();
+
+  const tracking_id = process.env.REACT_APP_GA_TRACKING_ID;
+
+  if (tracking_id) {
+    ReactGA.initialize(tracking_id);
+    console.log("tracking");
+  } else {
+    console.log("not tracking");
+  }
 
   const providers: IComposeProps["providers"] = [
     [QueryClientProvider, { client: queryClient }],
